aa4b2a89ed431cd63b9e8d2bacf98f5fc0a040a8
wangz <wangz@tiup.us>
2015-11-24 14:57:34 +0800

    update更新

--- tag.js
+++ tag.js
@@ -1,15 +1,50 @@
 /**
  * Created by Lever on 2015/9/24.
+ *
+ * Update by Lever on 2015/11/24.
+ *
+ * 使用说明:
+ *页面中需要制定
+ *       <input ng-model="selected" add-tag="Array"/>
+ *       <div id="tag-container></div>
+ *
+ * 回车后确认输入，标签显示在tag-container中。
+ * 改进之后，加入了预判断，判读是否之前有数据；使用keypress代替keydown做事件触发。
+ * 之前使用keydown时，经常会出现回车之后input中输入内容不消失，而且keydown会经常跟别的组件(如：bootstrap-typeahead)发生冲突，效果总是不好。
+ * 查阅相关资料，了解到keypress适合用来处理文本输入，而keydown和keyup适合用来处理快捷键。
+ *
+ * 后续将要进行的改进：
+ * 增强可扩展性，使组件更加通用化，用起来更加方便简洁。
  */
     app.directive('addTag',function(){
         return{
             restrict: 'A',
+            scope:{
+                selected: '=ngModel',
+                tags: '=addTag'
+            },
             link: function(scope,element){
-                element.bind('keydown',function(event){
+                if(scope.tags){
+                    for(var key in scope.tags){
+                        $("#tag-container").append('<span class="tag label label-info" style="display:inline-block;width:auto;margin: 5px;background-color: #F09E84;line-height: 2em;"><span class="deptTag">'+scope.tags[key]+'</span><span style="margin-left: 10px;cursor: pointer;" class="remove-tag"><i class="fa fa-times"></i></span></span>');
+                    }
+                    $('.remove-tag').bind('click',function(){
+                        scope.tags.pop($(this).prev('.deptTag').text());
+                        $(this).parent().remove();
+                    })
+                }else{
+                    scope.tags = [];
+                }
+                element.bind('keypress',function(event){
                     if(event.keyCode == 13){
-                        element.next().append('<span class="tag label label-info" style="display:inline-block;width:auto;margin: 5px;background-color: #F09E84;line-height: 2em;">'+scope.dept+'<span style="margin-left: 10px;cursor: pointer;" class="remove-tag"><i class="fa fa-times"></i></span></span>');
-                        scope.dept = "";
+                        if(scope.selected && scope.selected !== ""){
+                            $("#tag-container").append('<span class="tag label label-info" style="display:inline-block;width:auto;margin: 5px;background-color: #F09E84;line-height: 2em;">'+scope.selected+'<span style="margin-left: 10px;cursor: pointer;" class="remove-tag"><i class="icon-icon_close"></i></span></span>');
+                            scope.tags.push(scope.selected);
+                            scope.selected = "";
+                            scope.$apply();
+                        }
                         $('.remove-tag').bind('click',function(){
+                            scope.tags.pop($(this).prev('.deptTag').text());
                             $(this).parent().remove();
                         })
                     }
